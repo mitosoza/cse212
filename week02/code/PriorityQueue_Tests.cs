365d4d0256424ef189257b3b358f7af49b30446a
mitosoza <msozagonzalez@byupathway.edu>
2025-09-23 20:39:53 -0600

    W02 Code: Queues   - Taking Turns Queue   - Priority Queue

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,70 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Create a queue with the following people and priorities: Peter (1), Tim (5), Sue (3), and Bob (2)
+    // Expected Result: Tim, Sue, Bob, Peter
+    // Defect(s) Found: 0
+    public void TestPriorityQueue_HighestPriorityFirst()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Peter", 1);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Sue", 3);
+        priorityQueue.Enqueue("Bob", 2);
+
+        Assert.AreEqual("Tim", priorityQueue.Dequeue());
+        Assert.AreEqual("Sue", priorityQueue.Dequeue());
+        Assert.AreEqual("Bob", priorityQueue.Dequeue());
+        Assert.AreEqual("Peter", priorityQueue.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Create a queue with the following people and priorities: Bob (2), Tim (5), Sue (3), John (5), and Peter (1)
+    // Expected Result: Tim, John, Sue, Bob, Peter
+    // Defect(s) Found: 0
+    public void TestPriorityQueue_DuplicatedPriorities()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Bob", 2);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Sue", 3);
+        priorityQueue.Enqueue("John", 5);
+        priorityQueue.Enqueue("Peter", 1);
+
+        Assert.AreEqual("Tim", priorityQueue.Dequeue());
+        Assert.AreEqual("John", priorityQueue.Dequeue());
+        Assert.AreEqual("Sue", priorityQueue.Dequeue());
+        Assert.AreEqual("Bob", priorityQueue.Dequeue());
+        Assert.AreEqual("Peter", priorityQueue.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Try to get the next person from an empty queue
+    // Expected Result: Exception should be thrown with appropriate error message.
+    // Defect(s) Found: 0
+    public void TestPriorityQueue_EmptyQueue()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                string.Format("Unexpected exception of type {0} caught: {1}",
+                              e.GetType(), e.Message)
+            );
+        }
+    }
 }
